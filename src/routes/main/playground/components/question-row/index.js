8f43bdfa6cb27e77aaa84139d0da3304e92a172e
Konstantin Rodionov <kr@macronomica.com>
2016-07-01 21:02:36 +0300

    Нарисовал полоски

--- src/routes/main/playground/components/question-row/index.js
+++ src/routes/main/playground/components/question-row/index.js
@@ -4,7 +4,21 @@ import './question-row.less';
 const QuestionRow = ({ number, isOpen, name, score }) =>
   <div className={ `c-question-row ${ isOpen ? 'open' : '' }` }>
     <div className='front'>
+      <div className='left'>
+        <div className='line line-5'></div>
+        <div className='line line-4'></div>
+        <div className='line line-3'></div>
+        <div className='line line-2'></div>
+        <div className='line line-1'></div>
+      </div>
       <span className='text'>{ number }</span>
+      <div className='right'>
+        <div className='line line-1'></div>
+        <div className='line line-2'></div>
+        <div className='line line-3'></div>
+        <div className='line line-4'></div>
+        <div className='line line-5'></div>
+      </div>
     </div>
     <div className='back'>
       <span className='text'>{ name }</span>
