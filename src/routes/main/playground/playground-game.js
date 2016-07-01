ac95ad944a5bb2a596c20f85e6a7e69206973ecb
Konstantin Rodionov <kr@macronomica.com>
2016-07-01 20:38:32 +0300

    Вывел очки команды

--- src/routes/main/playground/playground-game.js
+++ src/routes/main/playground/playground-game.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Questions from './components/questions';
 import Scoreboard from './components/scoreboard';
 import Team from './components/team';
+import Score from './components/score';
 import './playground.less';
 
 export default class PlaygroundGame extends Component {
@@ -16,6 +17,9 @@ export default class PlaygroundGame extends Component {
       <div className='game'>
         <Scoreboard className='score-row' score={ score }/>
         <div className='game-row'>
+          <div className='scores' style={ { position: 'absolute', left: '-200px', width: '150px' } }>
+            <Score score={ leftTeam.score }/>
+          </div>
           <Team className='column-left'
                 round={ roundNumber }
                 active={ team }
@@ -27,6 +31,9 @@ export default class PlaygroundGame extends Component {
                 active={ team }
                 team={ rightTeam }
                 errors={ rightErrors }/>
+          <div className='scores' style={ { position: 'absolute', right: '-200px', width: '150px' } }>
+            <Score score={ rightTeam.score }/>
+          </div>
         </div>
       </div>
     );
